e63f96702742f73bb9d94a04e107aa8d3e78deae
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-09-23 03:07:17 +0500

    blog

--- sanity/schema.js
+++ sanity/schema.js
@@ -4,6 +4,7 @@
 // import author from './schemas/author'
 
 import bestCourse from "./schemas/bestCourse";
+import blogs from "./schemas/blogs";
 import courseWeOffer from "./schemas/courseWeOffer";
 import header from "./schemas/header";
 import imageHeading from "./schemas/imageHeading";
@@ -13,5 +14,5 @@ import studentsSays from "./schemas/studentsSays";
 import virtualTour from "./schemas/virtualTour";
 
 export const schema = {
-  types: [majorPrograms,virtualTour,ourFacilities,studentsSays,imageHeading,header,courseWeOffer,bestCourse],
+  types: [majorPrograms,virtualTour,ourFacilities,studentsSays,imageHeading,header,courseWeOffer,bestCourse,blogs],
 }
